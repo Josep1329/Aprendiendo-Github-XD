47de250831a4479fd9e9aa25f06f14f88eb5127e
Josep1329 <Joseph.serratos@uniat.edu.mx>
2023-11-21 12:54:59 -0600

    Test bien vergas

---
+++ Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by josep on 21/11/2023.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+// Created by josep on 21/11/2023.
+//
+
+#ifndef APRENDIENDO_GITHUB_XD_TEST_H
+#define APRENDIENDO_GITHUB_XD_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //APRENDIENDO_GITHUB_XD_TEST_H
